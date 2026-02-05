1e4c6147daf0664723ae610fb9719fb89808f368
uvindu abhisheka <dharmasenaua@gmail.com>
2026-02-05 12:18:46 +0530

    Created the folder structure

---
+++ src/test/java/com/example/projecctbodima/ProjecctBodimaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projecctbodima;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjecctBodimaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
